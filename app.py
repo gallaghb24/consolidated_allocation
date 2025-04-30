cc9c4f0a20018827ac7b4e478a959dc5bf1fae0d
gallaghb24 <ben@gallag.co.uk>
2025-04-30 21:43:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,12 @@ st.title("Superdrug Consolidated Allocation Builder")
 
 st.markdown(
     """
-    **Step 1 – Upload all allocation exports together** – [download them here](https://superdrug.aswmediacentre.com/ArtworkPrint/ArtworkPrintReport/ArtworkPrintReport?reportId=1149)  
+**Step 1 – Upload all allocation exports together** – [download them here](https://superdrug.aswmediacentre.com/ArtworkPrint/ArtworkPrintReport/ArtworkPrintReport?reportId=1149)  
+**Step 2 – Upload the Consolidated Brief complete with Supplier for each line (optional)**  
+**Step 3 – Enter the Event Code (required)**  
+**Step 4 – Download the Consolidated Allocation**
+    """
+)  
     **Step 2 – Upload the Consolidated Brief complete with Supplier for each line (optional)**  
     **Step 3 – Enter the Event Code (required)**  
     **Step 4 – Download the Consolidated Allocation**
